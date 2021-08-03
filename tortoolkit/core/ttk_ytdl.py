4bd6da9ada018a593a6dff3ad1bdb241e0751f7a
priyalpaladiya <65066298+priyalpaladiya@users.noreply.github.com>
2021-08-03 20:09:02 +0530

    Update ttk_ytdl.py

--- tortoolkit/core/ttk_ytdl.py
+++ tortoolkit/core/ttk_ytdl.py
@@ -63,7 +63,7 @@ async def cli_call(cmd: Union[str, List[str]]) -> Tuple[str, str]:
 async def get_yt_link_details(url: str) -> Union[Dict[str, str], None]:
     cmd = "youtube-dl --no-warnings --youtube-skip-dash-manifest --dump-json"
     cmd = shlex.split(cmd)
-    if "hotstar" in url:
+    if "hotstar" in url or "sonyliv" in url or "m3u8" in url:
         cmd.append("--geo-bypass-country")
         cmd.append("IN")
     cmd.append(url)
